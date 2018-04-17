1619b400586e066e5bc8b02d611e2f3517e78233
jiafeimao <1157873987@qq.com>
2018-04-17 15:41:05 +0000

    Author can delete topic

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -57,13 +57,13 @@ public function edit(Topic $topic)
 		return redirect()->route('topics.show', $topic->id)->with('success', '更新成功');
 	}
 
-	public function destroy(Topic $topic)
-	{
-		$this->authorize('destroy', $topic);
-		$topic->delete();
+    public function destroy(Topic $topic)
+    {
+        $this->authorize('destroy', $topic);
+        $topic->delete();
 
-		return redirect()->route('topics.index')->with('success', '成功删除');
-	}
+        return redirect()->route('topics.index')->with('success', '成功删除！');
+    }
 
 	public function uploadImage(Request $request, ImageUploadHandler $uploader)
     {
